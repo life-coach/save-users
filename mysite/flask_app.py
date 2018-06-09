1802a2acff63c73407cd04215d71f4a5102cbe3c
andrewnc <andrewcarr06@gmail.com>
2018-06-09 13:55:40 -0600

    update api

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,11 +1,15 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def hello_world():
-    return 'Hello from Flask!'
+	with open("emails.txt", "a") as myfile:
+		myfile.write(request.form['email'] + "\n")
+	return "thank you"
 
